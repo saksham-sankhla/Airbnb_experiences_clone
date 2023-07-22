eae75aeb72b994b145f849ecf5cbbae46b089329
saksham-sankhla <sakshamsankhala@gmail.com>
2023-07-22 17:29:21 +0530

    functional commit

---
+++ airbnb_clone/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img alt="logo" src="../images/airbnb-logo.png" className="nav--logo" />
+        </nav>
+    )
+}
---
+++ airbnb_clone/src/data.js
@@ -0,0 +1,106 @@
+export default [
+    {
+        id: 1,
+        title: "Life Lessons with Katie Zaferes",
+        description: "I will share with you what I call \"Positively Impactful Moments of Disappointment.\" Throughout my career, many of my highest moments only came after setbacks and losses. But learning from those difficult moments is what gave me the ability to rise above them and reach my goals.",
+        price: 136,
+        coverImg: "katie-zaferes.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 6
+        },
+        location: "Online",
+        openSpots: 0,
+    },
+    {
+        id: 2,
+        title: "Learn Wedding Photography",
+        description: "Interested in becoming a wedding photographer? For beginner and experienced photographers alike, join us in learning techniques required to leave the happy couple with memories that'll last a lifetime.",
+        price: 125,
+        coverImg: "wedding-photography.png",
+        stats: {
+            rating: 5.0,
+            reviewCount: 30
+        },
+        location: "Online",
+        openSpots: 27,
+    },
+    {
+        id: 3,
+        title: "Group Mountain Biking",
+        description: "Experience the beautiful Norwegian landscape and meet new friends all while conquering rugged terrain on your mountain bike. (Bike provided!)",
+        price: 50,
+        coverImg: "mountain-bike.png",
+        stats: {
+            rating: 4.8,
+            reviewCount: 2
+        },
+        location: "Norway",
+        openSpots: 3,
+    },
+    {
+        id: 4,
+        title: "Skybound Adventure: Hot Air Balloon",
+        description: "Soar to new heights and capture breathtaking views with our unforgettable hot air balloon ride experience.",
+        price: 120,
+        coverImg: "hot-air-baloon.jpg",
+        stats: {
+            rating: 4.5,
+            reviewCount: 12
+        },
+        location: "Egypt",
+        openSpots: 4,
+    },
+    {
+        id: 5,
+        title: "Diving with The Dolphins",
+        description: "Swim alongside playful dolphins and embrace the magic of the ocean with our thrilling dolphin diving experience.",
+        price: 80,
+        coverImg: "dolphin-diving.jpg",
+        stats: {
+            rating: 4.3,
+            reviewCount: 28
+        },
+        location: "Bali",
+        openSpots: 7,
+    },
+    {
+        id: 6,
+        title: "Vietnamese Coffee Delight: A Taste of Culture",
+        description: "Indulge in the rich and aromatic world of Vietnamese coffee, savoring its unique flavors and cultural charm.",
+        price: 42,
+        coverImg: "vietnam-coffee.jpg",
+        stats: {
+            rating: 4.8,
+            reviewCount: 61
+        },
+        location: "Vietnam",
+        openSpots: 11,
+    },
+    {
+        id: 7,
+        title: "Swiss Boating Bliss: Navigate Alpine Waters",
+        description: "Experience the serenity and beauty of Switzerland's Alpine waters with our enchanting Swiss boating adventure.",
+        price: 140,
+        coverImg: "swiss-boating.jpg",
+        stats: {
+            rating: 4.9,
+            reviewCount: 52
+        },
+        location: "Switzerland",
+        openSpots: 7,
+    },
+    {
+        id: 8,
+        title: "Icelandic Trekking Expedition: Nature's Wonderland",
+        description: "Embark on an unforgettable trekking adventure in Iceland, where breathtaking landscapes, volcanic terrains, and otherworldly wonders await.",
+        price: 210,
+        coverImg: "trekking.jpg",
+        stats: {
+            rating: 4.2,
+            reviewCount: 34
+        },
+        location: "Iceland",
+        openSpots: 12,
+    }
+]
